d46c88773873e5b05a6b295076f1393cca887ba4
John Lloyd <5355693@gmail.com>
2017-04-14 17:36:00 -0400

    Updated script
    Changed script so that zero counts are displayed as white.

--- attemptforMap.R
+++ attemptforMap.R
@@ -1,6 +1,7 @@
 rm(list=ls())
 #setwd('/Users/johnlloyd/GitHub/StateOfTheMountainBirds')
 
+#Read in the data:
 dfmap<-read.csv('WTSP_mapdata.csv',header=T,na.strings = "NA",stringsAsFactors=TRUE)
 
 str(dfmap)
@@ -12,6 +13,8 @@ library(ggmap)
 library(maps)
 library(mapdata)
 library(rgdal)
+
+#Get background polygons:
 mapdat <- map_data('state',stringsAsFactors=TRUE)
 states <- map_data("state",stringsAsFactors=TRUE)
 NewEngland <- subset(states, region %in% c("maine", "vermont", "new york","new hampshire"))
@@ -20,8 +23,10 @@ Neighborhoods <- readOGR(".","NewEnglandBoundaries")
 Neighborhoods <- spTransform(Neighborhoods, CRS("+proj=longlat +datum=WGS84"))
 Neighborhoods <- fortify(Neighborhoods)
 
+#Get the background for the map; here, google satellite image.
 WTSP.MBW.map = get_map(location = c(-72,44), color="color",maptype="satellite",source="google",zoom=5)
 
+#Functions taken from SO to create X and Y axes that are labelled correctly:
 scale_x_longitude <- function(xmin=-180, xmax=180, step=1, ...) {
   xbreaks <- seq(xmin,xmax,step)
   xlabels <- unlist(lapply(xbreaks, function(x) ifelse(x < 0, parse(text=paste0(x,"^o", "*W")), ifelse(x > 0, parse(text=paste0(x,"^o", "*E")),x))))
@@ -32,7 +37,19 @@ scale_y_latitude <- function(ymin=-90, ymax=90, step=0.5, ...) {
   ylabels <- unlist(lapply(ybreaks, function(x) ifelse(x < 0, parse(text=paste0(x,"^o", "*S")), ifelse(x > 0, parse(text=paste0(x,"^o", "*N")),x))))
   return(scale_y_continuous("Latitude", breaks = ybreaks, labels = ylabels, expand = c(0, 0), ...))
 }   
+
+#Create the map.
 q <- ggmap(WTSP.MBW.map) 
+
+#Set the color scheme for points. This version uses a reversed heat map,
+#with high counts in red and low counts in yellow. 
+Jcolors<-rev(heat.colors(n = 6))
+
+#To make it clear which points have 0 counts, change the lowest value 
+#in the color ramp to white:
+Jcolors[1]<-"white"
+
+#Add the points, study area polygons, and labelling:
 q <- q + 
   geom_point(data=dfmap, size=2, aes(y=Latitude, x=Longitude, color=Max.Count)) + 
   geom_polygon(aes(x=long, y=lat, group=group), fill='grey', size=.2,color='darkolivegreen2', data=Neighborhoods, alpha=0) + 
@@ -43,16 +60,12 @@ q <- q +
         axis.text.x = element_text(size = rel(1.5))) + coord_map(projection="mercator",xlim=c(-76.9, -66.9), ylim=c(40.8,47.8)) +
   theme(axis.title.y=element_text(size=rel(2),color="ROYALBLUE",angle = 90),
         axis.text.y = element_text(size = rel(1.5)))  + 
-  scale_color_gradientn(colours = rev(heat.colors(n = 6)), name = "Maximum\ncount")
+  #  scale_color_gradientn(colours = rev(heat.colors(n = 6)), name = "Maximum\ncount")
+  scale_color_gradientn(colours = Jcolors, name = "Maximum\ncount")
+#print it to the screen:
 q
 
-ggsave (filename = "WTSP_Counts.tiff", dpi = 200,device="tiff")
+#save it as a .tiff
 
-
-# I found this to be an easy way to get color names from brew.pal palettes
-
-brewer.pal(n = 6, name = "YlOrRd")
-#palette(brewer.pal(n = 8, name = "Set2"))
-
-
+ggsave(filename = "WTSP_map.tiff", device = "tiff")
 
